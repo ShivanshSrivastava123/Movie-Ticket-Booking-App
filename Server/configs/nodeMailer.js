885ffe7d79ec0fb28a52e469687de39351debdf9
ShivanshSrivastava123 <shivanshsrivastava9d41@gmail.com>
2026-06-15 21:20:46 +0530

    fix6

--- Server/configs/nodeMailer.js
+++ Server/configs/nodeMailer.js
@@ -3,7 +3,6 @@ import nodemailer from 'nodemailer'
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
-  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
